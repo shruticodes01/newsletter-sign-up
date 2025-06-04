d5af8fba9813adcbd43888594310711a6ce0e76e
Shruti <dev.shruti27@gmail.com>
2025-06-04 14:23:19 +0200

    add project files

---
+++ main.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const email = document.querySelector("#email");
+const emailLabel = document.querySelector(".email__label");
+const btnSubmit = document.querySelector("#submit-btn");
+const subscribeBtn = document.querySelector(".subscribe__btn");
+const span = document.querySelector("#error-text");
+const btnDismiss = document.querySelector("#dismiss-btn");
+const replaceContainer = document.querySelector("#replace");
+const updateDialogEmail = document.querySelector("#user-email");
+const dialog = document.querySelector("#success-dialog");
+dialog.returnValue = "email";
+
+const replaceTimer = function () {
+  replaceContainer.classList.add("hidden");
+  dialog.classList.remove("hidden");
+};
+
+const isValidEmail = function (userInput) {
+  //   const userInput = email.value;
+  const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;
+  const validity =
+    (userInput.length !== 0 || userInput !== null || userInput !== " ") &&
+    emailRegExp.test(userInput);
+  return validity;
+};
+
+const validationHandler = function () {
+  //   const emailInput = isValidEmail();
+  const isValid = isValidEmail(email.value);
+  if (!isValid) {
+    span.classList.remove("hidden");
+    span.style.color = "hsl(4, 100%, 67%)";
+  } else {
+    span.classList.add("hidden");
+    subscribeBtn.classList.add("success__btn--bg");
+
+    setTimeout(replaceTimer, 300);
+    updateDialogEmail.href = "mailto:" + email.value;
+    updateDialogEmail.innerHTML = email.value;
+  }
+};
+
+const handleInput = () => {
+  validationHandler();
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  validationHandler();
+};
+
+email.addEventListener("change", handleInput);
+btnSubmit.addEventListener("click", handleSubmit);
+btnDismiss.addEventListener("click", function () {
+  dialog.classList.add("hidden");
+  replaceContainer.classList.remove("hidden");
+  subscribeBtn.classList.remove("success__btn--bg");
+});
